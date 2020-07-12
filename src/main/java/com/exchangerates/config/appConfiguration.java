ef05d837370a4d9af7b31599fcf75b5fe11a81f9
ntepp <ntepp.marcus@gmail.com>
2020-07-12 21:29:33 +0400

    adding heroku variable

--- src/main/java/com/exchangerates/config/appConfiguration.java
+++ src/main/java/com/exchangerates/config/appConfiguration.java
@@ -1,12 +1,21 @@
 package com.exchangerates.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 @Configuration
 public class appConfiguration {
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+	
 	@Bean
 	JedisConnectionFactory jedisConnectionFactory() {
 	    JedisConnectionFactory jedisConFactory = new JedisConnectionFactory();
@@ -19,4 +28,11 @@ public class appConfiguration {
         template.setConnectionFactory(jedisConnectionFactory());
         return template;
     }
+	
+	@Bean
+	  public DataSource dataSource() {
+	      HikariConfig config = new HikariConfig();
+	      config.setJdbcUrl(dbUrl);
+	      return new HikariDataSource(config);
+	  }
 }
