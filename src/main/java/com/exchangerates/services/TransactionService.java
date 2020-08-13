e0c2e76d1de9c50c11471eaaaa378b9827216190
ntepp <ntepp.marcus@gmail.com>
2020-08-13 19:37:57 +0400

    hide application-local inital val

--- src/main/java/com/exchangerates/services/TransactionService.java
+++ src/main/java/com/exchangerates/services/TransactionService.java
@@ -15,7 +15,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import com.exchangerates.entities.Rate;
 import com.exchangerates.entities.Transaction;
 import com.exchangerates.repositories.RedisRateRepository;
 import com.exchangerates.repositories.TransactionRepository;
@@ -25,43 +24,41 @@ import com.exchangerates.utility.ExchangeStatic;
 public class TransactionService {
 	Logger logger = LoggerFactory.getLogger(TransactionService.class);
 	RestTemplate restTemplate = new RestTemplate();
-	
+
 	@Autowired
 	TransactionRepository transactionRepository;
-	
+
 	@Autowired
-    private RedisRateRepository redisRepository;
-	
+	private RedisRateRepository redisRepository;
+
 	public Transaction getRate(Transaction tr) {
 		logger.info("rate {}", tr);
 		String rFromApi = null;
-		String apiUrl = ExchangeStatic.apiUrl+tr.getBaseCurrency();
+		String apiUrl = ExchangeStatic.apiUrl + tr.getBaseCurrency();
 		ResponseEntity<String> response = null;
-		
+
 		String key = tr.getBaseCurrency();
 		rFromApi = redisRepository.findById(key);
 		logger.info("exchangeratesapi.io response {}", rFromApi);
-		if(Objects.isNull(rFromApi)) {
-			response = restTemplate.getForEntity(apiUrl , String.class);
+		if (Objects.isNull(rFromApi)) {
+			response = restTemplate.getForEntity(apiUrl, String.class);
 			rFromApi = response.getBody().toString();
 			redisRepository.save(key, rFromApi);
 		}
-		
+
 		JSONObject resultJ = new JSONObject(rFromApi);
 		BigDecimal baseAmounExch = new BigDecimal(1.0);
 		BigDecimal targetAmounExch = resultJ.getJSONObject("rates").getBigDecimal(tr.getTargetCurrency());
 		tr.setExchangeRate(targetAmounExch.divide(baseAmounExch, 5, RoundingMode.HALF_UP));
 		tr.setTargetAmount(tr.getExchangeRate().multiply(tr.getBaseAmount()));
 		tr.setDate(new Date());
-		
+
 		transactionRepository.save(tr);
-	
-		
-		
+
 		return tr;
 	}
-	
-	public List<Transaction> getAll(){
+
+	public List<Transaction> getAll() {
 		List<Transaction> transactionL = new ArrayList<Transaction>();
 		transactionRepository.findAll().iterator().forEachRemaining(transactionL::add);
 		return transactionL;
